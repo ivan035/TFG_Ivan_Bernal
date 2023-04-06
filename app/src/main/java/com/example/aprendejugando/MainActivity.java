1c9b84aa19b8db647dfc9326515f58ed50ee915a
ivan <xxivan304xx@gmail.com>
2023-04-06 03:29:50 +0200

    Pantalla de inicio e imagenes de perrito

--- app/src/main/java/com/example/aprendejugando/MainActivity.java
+++ app/src/main/java/com/example/aprendejugando/MainActivity.java
@@ -3,12 +3,19 @@ package com.example.aprendejugando;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.start_screen);
+    }
+
+    public void accion(View view) {
+        Toast t = Toast.makeText(this,"Iniciando...",Toast.LENGTH_SHORT);
+        t.show();
     }
 }
